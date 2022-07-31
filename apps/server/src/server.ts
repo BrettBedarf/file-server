97d4820077b1a17997faa1d1d1c2490ad9203354
brett <brettbedarf.dev@gmail.com>
2022-07-30 18:31:48 -0600

    ✨ express init

---
+++ apps/server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+require('dotenv').config();
+
+//startup
+// dotenv.config();
+const app = express();
+
+// Middleware to be able to use json
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('API is running....');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${port}`
+	)
+);
